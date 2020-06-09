54f2ae175e8f800f125940cccd2c4ca01781e6dd
jwillow <jeffcfwon19@hotmail.com>
2020-06-08 23:13:09 -0400

    moved notebook to python script

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,7 +1,13 @@
-'''
-Process the LP and returns a list of contours for each character
-'''
 import os
+import argparse
+import detectron2
+# from detectron2.utils.logger import setup_logger
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+import json
 from os.path import splitext, basename
 import glob
 
@@ -10,46 +16,149 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-from utils import detect_lp
+# Move functions into utils
+from utils import *
 
-import tensorflow as tf
-import keras
 from keras.models import model_from_json
+from keras.applications.mobilenet_v2 import preprocess_input
+from sklearn.preprocessing import LabelEncoder
 
 
-def process_image(image_path):
-    im = cv2.imread(image_path)
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    im = cv2.GaussianBlur(im, (7, 7), 0)
-    _, im = cv2.threshold(im, 120, 255, cv2.THRESH_BINARY_INV)
-
-    dilation_filter3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    im = cv2.dilate(im, dilation_filter3, iterations=1)
-
-    return im
+# Import libraries for car detection
+import torch
+import torchvision
+print(torch.__version__, torch.cuda.is_available())
+# setup_logger()
 
 
-def sort_contours(cnts, reverse=False):
+def extract_cars(output_instance, image):
     '''
-    Input (list of contours (list)) and grab each contour bbox 
+    Input: Original image (Input for FasterRCNN) and predicted output class Instance
+    Output: A list of dictionary of (key:id, val:cropped images of detected car)
     '''
-    i = 0
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
-    return cnts
+    bboxes = output_instance['instances'].pred_boxes
+
+    cars = []
+
+    for ind, instance in enumerate(bboxes.tensor.tolist()):
+        # pretty elegant eh? Round each coordinate to int and destructure
+        x, y, w, h = [round(num) for num in instance]
+        # Crop car instance and save to array
+        cropped_im = image[y:h, x:w]
+        car = {
+            'id': ind,
+            'image': cropped_im,
+            'car_bbox': [x, y, w, h]
+        }
+        cars.append(car)
+
+    return cars
 
 
-digit_w, digit_h = 30, 60
+def prune_empty(cars):
+    '''
+    Input: list of dictionarys
+    Output: modified input - list of dictionarys with no empty LP value
+    '''
+    for ind, car in enumerate(cars):
+        if car['lp'] is None:
+            cars.pop(ind)
+    return cars
 
-# I DONT THINK I SHOULD SAVE CONTOUR - INSTEAD GET CONTOUR IN REAL TIME GIVEN IMAGE
-for jpgfile in glob.iglob(os.path.join('../lp_out/', '*.jpg')):
 
-    crop_characters = []
+def get_chars(cars, model, labels):
+    '''
+    Function takes in a list of LPs and return a list of dictionary (key: id, val: nparray of chars)
+    Input: List of dictionary (of car instance), model for recognition, labels: class labels 
+    Output: Modify input - List of dictionary with chars
+    '''
+    for car in cars:
+        crop_characters = segmentation(car['lp'])
+        lp_string = predict_lp_chars(crop_characters, model, labels)
 
-    path = jpgfile
-    encoding = jpgfile.split('.')[2].split('/')[-1]
+        car['chars'] = lp_string
 
-    im = process_image(jpgfile)
-    contour, _ = cv2.findContours(
-        im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return cars
+
+
+def lp2box(image, cars):
+    '''
+    Input: An image original, np.ndarray; cars (list of dictionary with car instance information(id, lp, car_bbox, image, chars))
+    Output: An image, with bbox around cars and LP character text
+    '''
+
+    new_image = np.copy(image)
+
+    for car in cars:
+        lp_text = car['chars']
+        x, y, w, h = car['car_bbox']
+
+        # Initialize random color
+        color = (int(np.random.randint(100, 255, 1, int)), int(
+            np.random.randint(100, 255, 1, int)), int(np.random.randint(100, 255, 1, int)))
+        # Draw bbox to original image
+        new_image = cv2.rectangle(
+            new_image, (x, y), (w, h), color, thickness=2)
+        # Put string text on image
+        cv2.putText(new_image, lp_text, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1, color=color, thickness=2)
+
+    return new_image
+
+
+'''
+Pipeline
+1. Get a scene
+2. Run FasterRCNN on scene -> Save Coordinates of bbox instances -> Crop -> Save to array
+3. Pass array onto WPOD-NET, run model on each cropped instance
+4. Run OCR on each plate -> Save LP onto a list or hastable
+5. Append LP back to car bbox
+'''
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default='../car_detection/test/Car/be41c99204906f64.jpg',
+                        help='Path to input image')
+    parser.add_argument('--out', type=str, default='final.jpg',
+                        help='Name of output image')
+
+    args = parser.parse_args()
+
+    input_path, out_name = args.input, args.out
+    original = cv2.imread(input_path)
+    original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
+
+    # FasterRCNN - configure and predict
+    print('Configuring Detectron2...')
+    cfg, predictor = cfg_detectron()
+    outputs = predictor(original)
+    carInstance, _ = prune_class(outputs)
+
+    # Initialize list of car instances (list of dict)
+    cars = extract_cars(carInstance, original)
+    # WPOD-NET on car instances
+    print('Loading WPOD-NET...')
+    wpod_net = load_model('../model/wpod-net.json', '../model/wpod-net.h5')
+    cars = lp_recognition(cars, wpod_net)
+    # Remove car instances with empty LP from list of car instances
+    cars = prune_empty(cars)
+
+    # Segmentation and Recogition
+    print('Loading Recognition Module...')
+    path_to_arch = '../character_recognition/mobile_base.json'
+    path_to_weights = '../character_recognition/License_character_recognition.h5'
+    path_to_labels = '../character_recognition/license_character_classes.npy'
+    model, labels = load_recognition(
+        path_to_arch, path_to_weights, path_to_labels)
+    cars = get_chars(cars, model, labels)
+
+    # Mapping string back to original image
+    print('Mapping results to input')
+    new_image = lp2box(original, cars)
+    new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(out_name, new_image)
+
+
+if __name__ == '__main__':
+    main()
--- pipeline/utils.py
+++ pipeline/utils.py
@@ -1,6 +1,34 @@
 # pylint: disable=invalid-name, redefined-outer-name, missing-docstring, non-parent-init-called, trailing-whitespace, line-too-long
+import os
+import detectron2
+# from detectron2.utils.logger import setup_logger
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+import json
+from os.path import splitext, basename
+import glob
+
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+from keras.models import model_from_json
+from keras.applications.mobilenet_v2 import preprocess_input
+from sklearn.preprocessing import LabelEncoder
+
+
+# Import libraries for car detection
+import torch
+import torchvision
+print(torch.__version__, torch.cuda.is_available())
+# setup_logger()
+
+# import some common detectron2 utilities
 
 
 class Label:
@@ -220,3 +248,309 @@ def detect_lp(model, I, max_dim, lp_threshold):
     except TypeError as e:
         print('(detect_lp) Caught TypeError: ', e)
         pass
+
+
+# Pipeline functions
+'''
+Car Detection Module
+'''
+
+
+def cfg_detectron():
+    '''
+    Detectron2 configuration
+    Output: Returns a predictor using FasterRCNN
+    '''
+    cfg = get_cfg()
+    # Go to model_zoo and choose a config file for a model - Go to GitHub
+    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+    cfg.merge_from_file(model_zoo.get_config_file(
+        "COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+        "COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    return cfg, predictor
+
+
+def prune_class(outputs):
+    '''
+    Function prunes classes from predicted output to only contain class 'car'
+    Input: predicted output
+    Output: NewInstance with only class, scores, bboxes from car instances
+    '''
+    # 1. Make variable reference to output
+    img_size = outputs["instances"].image_size
+    cls = outputs["instances"].pred_classes
+    scores = outputs['instances'].scores
+    boxes = outputs['instances'].pred_boxes
+
+    # 2. Remove non-car classes - car class = 2?
+    # flatten tensor to list
+    cls_to_list = cls.flatten().tolist()
+    # list comprehension to build list with index wherever value != 2 in class list
+    indx_to_remove = [ind if val != 2 else None for (
+        ind, val) in enumerate(cls_to_list)]
+    # filter None and return list
+    indx_to_remove = list(filter(lambda x: isinstance(x, int), indx_to_remove))
+
+    # 3. Delete corresponding arrays
+    cls = np.delete(cls.cpu().numpy(), indx_to_remove)
+    scores = np.delete(scores.cpu().numpy(), indx_to_remove)
+    # WORKAROUND for numpy.ndarray - loops through boxes.tensor and remove tensor
+    # index-wise, stack bbox tensors back into one tensor
+    car_boxes = []
+    for ind, tensor in enumerate(boxes.tensor):
+        if ind not in indx_to_remove:
+            car_boxes.append(tensor)
+        else:
+            continue
+
+    # Initialize cars_list: a list of dictionary to store all cars instances
+    cars_list = []
+    try:
+        car_box_tensor = torch.stack(car_boxes)
+        bbox_list = car_box_tensor.tolist()
+
+        # for-loop to save imgsize, cls, scores, car bbox instances
+        for ind, instance in enumerate(cls):
+            bbox = bbox_list[ind]
+            x, y, w, h = bbox
+
+            car = {
+                #             'imgPath': jpgfile,
+                #             'imgEncode': imgEncoding,
+                'class': 'car',
+                'x': x,
+                'y': y,
+                'w': w,
+                'h': h,
+            }
+
+            cars_list.append(car)
+
+        # 4. Convert back to tensor and move to cuda
+        cls = torch.tensor(cls).to('cuda:0')
+        scores = torch.tensor(scores).to('cuda:0')
+        boxes.tensor = car_box_tensor
+
+        # 5. Create new instance object and set its fields
+        obj = detectron2.structures.Instances(image_size=img_size)
+        obj.set('pred_classes', cls)
+        obj.set('scores', scores)
+        obj.set('pred_boxes', boxes)
+
+        newInstance = {'instances': obj}
+
+        return newInstance, cars_list
+
+   # RuntimeError when torch.stack fail to stack empty car_box_tensor array
+    except RuntimeError:
+
+        return {}, cars_list
+
+
+'''
+WPOD-NET MODULE
+# preprocess_input (Preprocess car image input)
+# detect_plate (detects LP given an image)
+# load_model (loads pretrained wpod-net)
+'''
+
+
+def load_model(model_path, model_weights):
+    '''
+    Input (STR): path to model.json, model.h5
+    reads the model path, get model architecture from .json then load
+    the weights from .h5 
+    '''
+    try:
+        # parse json string to initialize model instance
+        with open(model_path, 'r') as json_file:
+            model_json = json_file.read()
+        model = model_from_json(model_json, custom_objects=None)
+        # load model weights
+        model.load_weights(model_weights)
+        print('Loaded WPOD-NET successfully...')
+
+        return model
+    except Exception as e:
+        print(e)
+
+
+def preprocess_input(image, resize=False):
+    '''
+    Function preprocess the image input before feeding it to WPOD-NET
+    Input: An image (np.ndarray), (Bool) resize
+    Output: processed version of image
+    '''
+    # im = cv2.imread(img_path)
+    # convert to RGB
+    im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # standardize pixels between 0 and 1
+    im = im / 255
+    # resize: if dim not (224,224)
+    if resize:
+        im = cv2.resize(im, (224, 224))
+    return im
+
+
+def detect_plate(image, model):
+    '''
+    Input: An image (np.ndarray)
+    Function calculates the Resizing-Factor mentioned in the paper
+    Dmin = 288 and Dmax = 608 are chosen such that it produces optimal
+        results between accuracy and running time
+    Output: Lp image and coordinates for detected LP
+    '''
+    Dmax = 608
+    Dmin = 288
+    bbox = preprocess_input(image)
+    # get width:height image ratio
+    ratio = float(max(bbox.shape[:2]) / min(bbox.shape[:2]))
+    size = int(ratio*Dmin)
+    bound = min(size, Dmax)
+    # Detect LP with wpod-net
+    try:
+        _, LpImg, _, cor = detect_lp(model, bbox, bound, lp_threshold=0.5)
+        return LpImg, cor
+    except TypeError:
+        print('No LP detected, return None')
+        return None, None
+
+
+def lp_recognition(cars, model):
+    '''
+    Input: List of dictionarys (key:id, val:np.ndarray images)
+    Output_new: modify the cars array with additional LP field in dictionary
+    Output: List of dictionarys np.ndarray images of detected LPs
+    '''
+    for car in cars:
+        try:
+            im = car['image']
+            LpImg, _ = detect_plate(im, model)
+            car['lp'] = LpImg[0]
+
+        except TypeError:
+            car['lp'] = None
+
+    return cars
+
+
+'''
+OCR Module
+# Modified MobileNetv2 Module: Preprocess -> get contours -> sort contours -> cropped characters
+# process_lp (Preprocess LP for segmentation)
+# sort_contours (Sort a list of contours)
+'''
+
+
+def process_lp(image):
+    '''
+    Function blurs, de-noise, binary, dilate image
+    Input: np.ndarray
+    '''
+    # Convert from (0,1) to (1,255)
+    im = cv2.convertScaleAbs(image, alpha=(255.0))
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im = cv2.GaussianBlur(im, (5, 5), 0)
+#     _, im = cv2.threshold(im, 120, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+#     _, im = cv2.threshold(im, 120, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    im = cv2.adaptiveThreshold(
+        im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 1)
+
+    dilation_filter3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    im = cv2.dilate(im, dilation_filter3, iterations=1)
+
+    return im
+
+
+def sort_contours(cnts, reverse=False):
+    '''
+    Function sorts a list of contours for each segmented character
+    Input (list of contours (list)) and grab each contour bbox 
+    '''
+    i = 0
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+    return cnts
+
+
+def segmentation(image, ratio_lower=1, ratio_upper=5):
+    '''
+    Function enbodies all procedures in character segmentation
+    Input: An image (np.ndarray)
+    Output: a list of cropped image characters 
+    '''
+    # Perform image processing on input LP
+    processed_im = process_lp(image)
+    contour, _ = cv2.findContours(
+        processed_im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Initialize a list which will be used to append charater image
+    crop_characters = []
+    digit_w, digit_h = 30, 60
+
+    # Loop through the list of contours to get bounding box on character
+    for c in sort_contours(contour):
+        (x, y, w, h) = cv2.boundingRect(c)
+        ratio = h/w
+
+#         print(ratio)
+
+        # Select contours that satisfy both conditions
+        if ratio_lower <= ratio <= ratio_upper and h/processed_im.shape[0] >= 0.2:
+
+            # Seperate number and give prediction
+            curr_num = processed_im[y:y+h, x:x+w]
+            curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
+            _, curr_num = cv2.threshold(curr_num, 220, 255, cv2.THRESH_BINARY)
+            crop_characters.append(curr_num)
+
+    return crop_characters
+
+# Load Recognition model architecture, weights and classes
+
+
+def load_recognition(json_model, model_weights, labels_file):
+    '''
+    Function: Loads recognition model with architecture and weights, and load class labels
+    Input: path to model architecture, weights and class labels files
+    '''
+    with open(json_model, 'r') as f:
+        model_json = json.load(f)
+
+    model = model_from_json(model_json)
+    model.load_weights(model_weights)
+
+    labels = LabelEncoder()
+    labels.classes_ = np.load(labels_file)
+
+    return model, labels
+
+
+def predict_lp_chars(crop_characters, model, labels):
+    '''
+    Function: Takes a list of contours and run recognition on each character, return a concatenated string 
+        of each detected character
+    Input: list of contours from segmentation module
+    Output: string representing the detected contour characters
+    '''
+    lp_chars = ''
+
+    for i, char in enumerate(crop_characters):
+
+        # Preprocess image to feed to model: resize, stack dimensions, add newaxis
+        # Model expected input: (m, w, h. c)
+        im = cv2.resize(char, (128, 128))
+        im = np.stack((im,)*3, axis=-1)
+        im = im[np.newaxis, :]
+
+        result = labels.inverse_transform([np.argmax(model.predict(im))])
+
+        detected_char = np.array2string(result)
+        detected_char = detected_char.strip("'[]")
+        lp_chars += detected_char
+
+    return lp_chars
